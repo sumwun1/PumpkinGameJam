63289a1e0a3b63590d088468be8e6dfb106e28e7
TheSevenSages <92613218+TheSevenSages@users.noreply.github.com>
2021-10-15 23:12:46 -0400

    Added Acceleration to player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    public float maxVelocity;
+    public float acceleration;
+    private float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 0;
     }
 
     // Update is called once per frame
@@ -22,24 +24,67 @@ public class Player : MonoBehaviour
 
         if (Input.GetKey("up"))
         {
-            //print("up arrow key is held down");
+            if (speed < maxVelocity)
+            {
+                speed += acceleration;
+            }
             transform.Translate(up * speed * Time.deltaTime, Space.World);
+        }
+        else if (!Input.GetKey("right") && !Input.GetKey("left") && !Input.GetKey("down"))
+        {
+            if (speed > 0)
+            {
+                speed -= acceleration;
+            }
         }
 
         if (Input.GetKey("down"))
         {
-            //print("down arrow key is held down");
+            if (speed < maxVelocity)
+            {
+                speed += acceleration;
+            }
             transform.Translate(down * speed * Time.deltaTime, Space.World);
         }
+        else if (!Input.GetKey("left") && !Input.GetKey("right") && !Input.GetKey("up"))
+        {
+            if (speed > 0)
+            {
+                speed -= acceleration;
+            }
+        }
+        
 
         if (Input.GetKey("left"))
         {
+            if (speed < maxVelocity)
+            {
+                speed += acceleration;
+            }
             transform.Translate(left * speed * Time.deltaTime, Space.World);
+        }
+        else if (!Input.GetKey("right") && !Input.GetKey("up") && !Input.GetKey("down"))
+        {
+            if (speed > 0)
+            {
+                speed -= acceleration;
+            }
         }
 
         if (Input.GetKey("right"))
         {
+            if (speed < maxVelocity)
+            {
+                speed += acceleration;
+            }
             transform.Translate(right * speed * Time.deltaTime, Space.World);
+        }
+        else if (!Input.GetKey("left") && !Input.GetKey("up") && !Input.GetKey("down"))
+        {
+            if (speed > 0)
+            {
+                speed -= acceleration;
+            }
         }
     }
 }
