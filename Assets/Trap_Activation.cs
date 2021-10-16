c71df0effaa66314484b91e4553455d38c65cc7e
Gzhang-umich <82591005+Gzhang-umich@users.noreply.github.com>
2021-10-16 10:16:07 +0800

    Add files via upload
    Trap_1 for press the button. Trap_Activation for clicking Z while the player is near the trap(have a CollisionStay.)

---
+++ Assets/Trap_Activation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_Activation : MonoBehaviour
+{
+    public GameObject activated_trap;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && collision.tag == "Player"){
+            activated_trap.SetActive(true);
+        }
+    }
+
+
+}
