d459972f1c5aa895bc8e402bb560ff0c55591f53
anAwsomePerson <an.awsome.person.99@gmail.com>
2021-10-15 23:38:36 -0400

    added placeholder sprites

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,20 +7,27 @@ public class Player : MonoBehaviour
     public float maxVelocity;
     public float acceleration;
     private float speed;
+    Vector3 zeroVector;
+    Vector3 up;
+    Vector3 down;
+    Vector3 left;
+    Vector3 right;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 0;
+        zeroVector = new Vector3(0, 0, 0);
+        up = new Vector3(0, 1, 0);
+        down = new Vector3(0, -1, 0);
+        left = new Vector3(-1, 0, 0);
+        right = new Vector3(1, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 up = new Vector3(0, 1);
-        Vector3 down = new Vector3(0, -1);
-        Vector3 left = new Vector3(-1, 0);
-        Vector3 right = new Vector3(1, 0);
+        transform.eulerAngles = zeroVector;
         
         if (Input.GetKey("right") && Input.GetKey("up"))
         {
