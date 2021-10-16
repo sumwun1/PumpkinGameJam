c29e76d9f606e9470082e3649811133a6268380c
Gzhang-umich <82591005+Gzhang-umich@users.noreply.github.com>
2021-10-16 21:03:29 +0800

    Add files via upload

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    int speed = 2;
+    Vector3 zeroVector;
+    Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindObjectOfType<Player>().GetComponent<Transform>();
+        zeroVector = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // print(this.transform.rotation);
+        transform.eulerAngles = zeroVector;
+        if (target.GetComponent<Player>().GetState() == 1)
+        {
+            return;
+        }
+        // print(this.transform.rotation);
+        Vector3 dir = target.position - this.transform.localPosition;;
+        //Debug.Log(speed);
+        float distThisFrame = speed * Time.deltaTime;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        // transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
+        /*Griefer[] griefers = GameObject.FindObjectsOfType<Griefer>();
+        Griefer nearestGriefer = null;
+
+        foreach (Griefer griefer in griefers)
+        {
+            //float d = Vector3.Distance(this.transform.position, griefer.transform.position);
+
+            if (nearestGriefer == null)
+            {
+                nearestGriefer = griefer;
+            }
+        }
+
+        if (nearestGriefer == null)
+        {
+            return;
+        }*/
+
+        if (dir.magnitude <= distThisFrame)
+        {
+            target.gameObject.GetComponent<Player>().Die();
+        }
+        else
+        {
+            transform.Translate(dir.normalized * distThisFrame, Space.World);
+            // print("transform.rotation:");
+            // print(transform.rotation);
+            // print("transform.position:");
+            // print(transform.position);
+            // print(Quaternion.LookRotation(dir));
+            // this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 0.3f);
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            // this.transform.rotation = Quaternion.LookRotation(dir);
+            // transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0f, 0f);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyDoor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDoor : MonoBehaviour
+{
+    public GameObject enemy;
+    public Vector3 startPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Quaternion randomRotation = Quaternion.Euler(Random.Range(0f, 360f), Random.Range(0f, 360f), 0f);
+        // Instantiate(enemy, startPosition, randomRotation);
+        var Enemy = Instantiate(enemy);
+        Enemy.transform.position = new Vector3(Random.Range(-10f, 30f), Random.Range(10f, 30f), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
